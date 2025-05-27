6be1a853e64487fc05aeea41a2665c1daa9a177b
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-05-26 18:50:01 -0600

    Add @nestjs/typeorm dependency and configure TypeORM in app.module.ts for PostgreSQL integration; update package.json and package-lock.json accordingly.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
@@ -8,7 +9,24 @@ import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, UsersModule, SettingsModule, ProductsModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'clase9',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+    TasksModule,
+    UsersModule,
+    SettingsModule,
+    ProductsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
