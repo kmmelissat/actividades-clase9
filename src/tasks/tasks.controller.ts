25f18a47491aef7476127b83fedca5d5d4441975
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-06-03 11:05:58 -0600

    ♻️ Refactor TasksController to utilize user ID from JWT in task creation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,36 +1,45 @@
-import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
-
 @UseGuards(JwtAuthGuard)
 @Controller('tasks')
 export class TasksController {
-    constructor(private readonly tasksService: TasksService) {}
+  constructor(private readonly tasksService: TasksService) {}
 
-    @Post()
-    create(@Body() body: { titulo: string, userId: number }): Promise<Task> {
-        return this.tasksService.createTask(body.titulo, body.userId);
-    }
+  @Post()
+  create(@Body() body: { titulo: string }, @Req() req): Promise<Task> {
+    return this.tasksService.createTask(body.titulo, req.user.sub);
+  }
 
-    @Get()
-    findAll(): Promise<Task[]> {
-        return this.tasksService.findAll();
-    }
+  @Get()
+  findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
 
-    @Get(':id')
-    findById(@Param('id') id: string): Promise<Task> {
-        return this.tasksService.findById(+id);
-    }
+  @Get(':id')
+  findById(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.findById(+id);
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() body: Partial<Task>): Promise<Task> {
-        return this.tasksService.updateTask(+id, body);
-    }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: Partial<Task>): Promise<Task> {
+    return this.tasksService.updateTask(+id, body);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string): Promise<void> {
-        return this.tasksService.deleteTask(+id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.tasksService.deleteTask(+id);
+  }
 }
